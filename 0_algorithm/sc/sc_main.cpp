41fcac6e3cf4461d70785f16e3c8962b970a8f7a
GoodKook <goodkook@gmail.com>
2024-03-26 12:45:31 +0900

    Added

---
+++ 0_algorithm/sc/sc_main.cpp
@@ -0,0 +1,12 @@
+#include "sc_fir8_tb.h"
+
+int sc_main(int argc, char** argv)
+{
+    sc_fir8_tb u_sc_fir8_tb("u_sc_fir8_tb");
+    
+    //sc_start(990, SC_US);
+    sc_start();
+
+    return 0;
+}
+
